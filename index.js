c9711ac2dd47e9b00cffdb75cbf52ddcedb36c01
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-27 20:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
 function saturdayFun(){
-  return "This Satrurday, I want to go out with my friends"
+  return "This Satrurday, I want to go out with my friends";
+}
+
+function mondayWork(){
+  return "This Monday, I will go to work early";
 }

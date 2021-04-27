ddf82f49813b538825e589f79358726a4d938725
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-27 20:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Your code here
 function saturdayFun(r= "roller-skate"){
-  return "This Satrurday, I want ${r}";
+  return `This Satrurday, I want ${r}`;
 }
 
 function mondayWork(s="go to the office"){
-  return "This Monday, I will ${s}";
+  return `This Monday, I will ${s}`;
 }
 
 function wrapAdjective(effect = "*"){
   return function(a="special"){
-      return "You are ${effect}${a}${effect}";    
+      return `You are ${effect}${a}${effect}`;    
   }
 }
